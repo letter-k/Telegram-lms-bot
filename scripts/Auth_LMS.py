9e97ce254203a9409c584abe7071d92cae293c6b
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-12-11 00:41:55 +0300

    correct lms class

--- scripts/Auth_LMS.py
+++ scripts/Auth_LMS.py
@@ -1,69 +1,45 @@
 from requests import Session
-from re import compile, findall, split
+from re import compile, sub
 from bs4 import BeautifulSoup as bs
-from fake_useragent import UserAgent
-from re import sub
+# from fake_useragent import UserAgent
+# from fp.fp import FreeProxy
+from aiohttp import ClientSession
 
 
 class LMS:
-    def __init__(self):
-        pass
+    session = None
+
+    def __init__(self, email, password):
+        self.create_session(email, password)
+
+    def __del__(self):
+        self.session = None
+
+    @classmethod
+    def create_session(cls, email, password):
+        cls.session = cls.__sign(email, password)
 
     @staticmethod
-    async def get_name(soup):
-        """Берем имя пользователя"""
-        name = soup.find("div", class_="user-name").text
-        reg = compile("[^а-яА-ЯёЁ0-9 ]")
-        name = reg.sub("", name).split(" ")
-
-        for i in name:
-            if i == "":
-                name.remove(i)
-            for x in name:
-                if x == "":
-                    name.remove(x)
-
-        name = " ".join(name)
-        return name
-
-    @staticmethod
-    async def get_notify(soup):
-        """Берём уведомления"""
-        try:
-            notify = soup.find("a", title="Уведомления").text
-            reg = compile("[^а-яА-ЯёЁ0-9 ]")
-            notify = reg.sub("", notify).replace(" ", "")
-            return notify
-        except AttributeError:
-            return "0"
-
-    @staticmethod
-    async def get_message(soup):
-        """Берём сообщения"""
-        try:
-            message = soup.find("a", title="Личные сообщения").text
-            reg = compile("[^а-яА-ЯёЁ0-9 ]")
-            message = reg.sub("", message).replace(" ", "")
-            return message
-        except AttributeError:
-            return "0"
-
-    @staticmethod
-    async def sign(email, password):
+    def __sign(email, password):
         """Авторизация"""
-        user = UserAgent().random
+        # user = UserAgent().random
+        # proxy = FreeProxy(https=False, anonym=True).get()
         url = "https://lms.synergy.ru/user/login"
         url1 = "https://lms.synergy.ru/schedule/academ"
 
-        headers = {"User-Agent": user}
+        # headers = {"User-Agent": user}
+
+        # proxies = {
+        #     'http': proxy
+        # }
 
         data = {"popupUsername": email, "popupPassword": password}
 
         session = Session()
-        session.headers.update(headers)
-        response = session.post(url, data=data)
+        # session.headers.update(headers)
+        session.post(url, data=data)
 
-        profile_responce = session.get(url1, headers=headers).text
+        session.get(url1).text
 
         cookies_dict = [
             {
@@ -80,58 +56,93 @@ class LMS:
         for cookies in cookies_dict:
             session2.cookies.set(**cookies)
 
-        resp = session2.get(url1, headers=headers)
+        resp = session2.get(url1)
         return resp
 
     @classmethod
-    async def acc_verify(cls, email, password):
-        """Проверка аккаунта"""
-        try:
-            soup = await cls.get_soup(email, password)
-            await cls.get_info_user(soup)
-            return True
-        except AttributeError:
-            return False
-
-    @classmethod
-    async def get_soup(cls, email, password):
+    def get_soup(cls):
         """Получаем суп"""
-        resp = await cls.sign(email, password)
+        resp = cls.session
         soup = bs(resp.text, "html.parser")
         return soup
 
     @classmethod
-    async def get_info_user(cls, soup):
+    def get_name(cls):
+        """Берем имя пользователя"""
+        name = cls.get_soup().find("div", class_="user-name").text
+        reg = compile("[^а-яА-ЯёЁ0-9 ]")
+        name = reg.sub("", name).split(" ")
+
+        for i in name:
+            if i == "":
+                name.remove(i)
+            for x in name:
+                if x == "":
+                    name.remove(x)
+
+        name = " ".join(name)
+        return name
+
+    @classmethod
+    def get_notify(cls):
+        """Берём уведомления"""
+        try:
+            notify = cls.get_soup().find("a", title="Уведомления").text
+            reg = compile("[^а-яА-ЯёЁ0-9 ]")
+            notify = reg.sub("", notify).replace(" ", "")
+            return notify
+        except AttributeError:
+            return "0"
+
+    @classmethod
+    def get_message(cls):
+        """Берём сообщения"""
+        try:
+            message = cls.get_soup().find("a", title="Личные сообщения").text
+            reg = compile("[^а-яА-ЯёЁ0-9 ]")
+            message = reg.sub("", message).replace(" ", "")
+            return message
+        except AttributeError:
+            return "0"
+
+    @classmethod
+    def get_info_user(cls):
         """Получаем информацию о пользователе"""
-        info_user = soup.find("div", id="user-profile")
-        name = await cls.get_name(info_user)
-        notify = await cls.get_notify(soup)
-        message = await cls.get_message(soup)
-        data = {"name": name, "notify": notify, "message": message}
+        name = cls.get_name()
+        notify = cls.get_notify()
+        message = cls.get_message()
+        data = {
+            "name": name,
+            "notify": notify, 
+            "message": message
+        }
         return data
 
     @classmethod
-    async def get_soup_info(cls, email, password):
-        """Получаем информацию о пользователе полсе авторизации"""
-        soup = await cls.get_soup(email, password)
-        return await cls.get_info_user(soup)
+    def acc_verify(cls):
+        """Проверка аккаунта"""
+        try:
+            cls.get_info_user()
+            return True
+        except AttributeError:
+            return False
 
     @staticmethod
-    async def split_by_pattern(string, pattern):
+    def split_by_pattern(string, pattern):
         return list(filter(None, sub(pattern, r'@@\1', string).split('@@')))
 
     @classmethod
-    async def split_by_pattern2(cls, list):
+    def split_by_pattern2(cls, list):
         schedule = []
         for i in list:
-            schedule.append((i[:11], await cls.split_by_pattern(i[12:], r"(\d{2}:\d{2} \d{2}:\d{2})")))
+            schedule.append((i[:11], cls.split_by_pattern(i[12:], r"(\d{2}:\d{2} \d{2}:\d{2})")))
 
         return schedule
 
     @classmethod
-    async def schedule(cls, soup):
+    def schedule(cls):
         """Получаем расписание"""
-        schedule = soup.find("table", class_="table-list v-scrollable").text
+        schedule = cls.get_soup().find("table", class_="table-list v-scrollable").text
         schedule = schedule.replace("Время\nКурс\nМесто проведения\nВид занятия\nПреподаватель", "")
         reg = compile("[^а-яА-ЯёЁ0-9.: ]")
         schedule = reg.sub("", schedule).split(" ")
@@ -143,14 +154,33 @@ class LMS:
                 if x == "":
                     schedule.remove(x)
         schedule = " ".join(schedule)
-        schedule = await cls.split_by_pattern(schedule, r'(([\d]{2}\.){2}[\d]{2})')
-        schedule = await cls.split_by_pattern2(schedule)
+        schedule = cls.split_by_pattern(schedule, r'(([\d]{2}\.){2}[\d]{2})')
+        schedule = cls.split_by_pattern2(schedule)
 
         return schedule
 
     @classmethod
-    async def get_schedule(cls, email, password):
+    def get_schedule(cls):
         """Получаем расписание пользователя"""
-        soup = await cls.get_soup(email, password)
-        schedule = await cls.schedule(soup)
+        schedule = cls.schedule()
+        if schedule == []:
+            return "Расписание пустое"
         return schedule
+
+    @classmethod
+    def get_today_schedule(cls):
+        """Получаем расписание на сегодня"""
+        schedule = cls.get_schedule()
+        try:
+            return schedule[0]
+        except IndexError:
+            return "Сегодня нет пар"
+    
+    @classmethod
+    def get_tomorrow_schedule(cls):
+        """Получаем расписание на сегодня"""
+        schedule = cls.get_schedule()
+        try:
+            return schedule[1]
+        except IndexError:
+            return "Сегодня нет пар"
