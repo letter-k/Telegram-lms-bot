60166d66e862f80b9a25806a58e24cd1beaea683
Kotorkovsciy <kotorkovsciy@gmail.com>
2023-01-29 19:06:32 +0300

    Refactoring parser class and documenting methods

--- scripts/Auth_LMS.py
+++ scripts/Auth_LMS.py
@@ -1,186 +1,314 @@
-from requests import Session
-from re import compile, sub
+from requests import Response, Session
 from bs4 import BeautifulSoup as bs
-# from fake_useragent import UserAgent
-# from fp.fp import FreeProxy
-from aiohttp import ClientSession
+from fake_useragent import UserAgent
+import typing
 
+
+class clean_data:
+    @staticmethod
+    def remove_many_spaces(string):
+        return " ".join(string.split())
 
 class LMS:
-    session = None
+    _URL: typing.Final[str] = "https://lms.synergy.ru"
+    _URL_LOGIN: typing.Final[str] = "%s/user/login" % _URL
+    _URL_SCHEDULE: typing.Final[str] = "%s/schedule/academ" % _URL
 
-    def __init__(self, email, password):
-        self.create_session(email, password)
-
-    def __del__(self):
-        self.session = None
-
-    @classmethod
-    def create_session(cls, email, password):
-        cls.session = cls.__sign(email, password)
-
-    @staticmethod
-    def __sign(email, password):
-        """Авторизация"""
-        # user = UserAgent().random
-        # proxy = FreeProxy(https=False, anonym=True).get()
-        url = "https://lms.synergy.ru/user/login"
-        url1 = "https://lms.synergy.ru/schedule/academ"
-
-        # headers = {"User-Agent": user}
-
-        # proxies = {
-        #     'http': proxy
-        # }
-
-        data = {"popupUsername": email, "popupPassword": password}
-
-        session = Session()
-        # session.headers.update(headers)
-        session.post(url, data=data)
-
-        session.get(url1).text
-
-        cookies_dict = [
-            {
-                "domain": key.domain,
-                "name": key.name,
-                "path": key.path,
-                "value": key.value,
-            }
-            for key in session.cookies
-        ]
-
-        session2 = Session()
-
-        for cookies in cookies_dict:
-            session2.cookies.set(**cookies)
-
-        resp = session2.get(url1)
-        return resp
-
-    @classmethod
-    def get_soup(cls):
-        """Получаем суп"""
-        resp = cls.session
-        soup = bs(resp.text, "html.parser")
-        return soup
-
-    @classmethod
-    def get_name(cls):
-        """Берем имя пользователя"""
-        name = cls.get_soup().find("div", class_="user-name").text
-        reg = compile("[^а-яА-ЯёЁ0-9 ]")
-        name = reg.sub("", name).split(" ")
-
-        for i in name:
-            if i == "":
-                name.remove(i)
-            for x in name:
-                if x == "":
-                    name.remove(x)
-
-        name = " ".join(name)
-        return name
-
-    @classmethod
-    def get_notify(cls):
-        """Берём уведомления"""
-        try:
-            notify = cls.get_soup().find("a", title="Уведомления").text
-            reg = compile("[^а-яА-ЯёЁ0-9 ]")
-            notify = reg.sub("", notify).replace(" ", "")
-            return notify
-        except AttributeError:
-            return "0"
-
-    @classmethod
-    def get_message(cls):
-        """Берём сообщения"""
-        try:
-            message = cls.get_soup().find("a", title="Личные сообщения").text
-            reg = compile("[^а-яА-ЯёЁ0-9 ]")
-            message = reg.sub("", message).replace(" ", "")
-            return message
-        except AttributeError:
-            return "0"
-
-    @classmethod
-    def get_info_user(cls):
-        """Получаем информацию о пользователе"""
-        name = cls.get_name()
-        notify = cls.get_notify()
-        message = cls.get_message()
-        data = {
-            "name": name,
-            "notify": notify, 
-            "message": message
-        }
-        return data
-
-    @classmethod
-    def acc_verify(cls):
-        """Проверка аккаунта"""
-        try:
-            cls.get_info_user()
-            return True
-        except AttributeError:
-            return False
-
-    @staticmethod
-    def split_by_pattern(string, pattern):
-        return list(filter(None, sub(pattern, r'@@\1', string).split('@@')))
-
-    @classmethod
-    def split_by_pattern2(cls, list):
-        schedule = []
-        for i in list:
-            schedule.append((i[:11], cls.split_by_pattern(i[12:], r"(\d{2}:\d{2} \d{2}:\d{2})")))
-
-        return schedule
-
-    @classmethod
-    def schedule(cls):
-        """Получаем расписание"""
-        schedule = cls.get_soup().find("table", class_="table-list v-scrollable").text
-        schedule = schedule.replace("Время\nКурс\nМесто проведения\nВид занятия\nПреподаватель", "")
-        reg = compile("[^а-яА-ЯёЁ0-9.: ]")
-        schedule = reg.sub("", schedule).split(" ")
-
-        for i in schedule:
-            if i == "":
-                schedule.remove(i)
-            for x in schedule:
-                if x == "":
-                    schedule.remove(x)
-        schedule = " ".join(schedule)
-        schedule = cls.split_by_pattern(schedule, r'(([\d]{2}\.){2}[\d]{2})')
-        schedule = cls.split_by_pattern2(schedule)
-
-        return schedule
-
-    @classmethod
-    def get_schedule(cls):
-        """Получаем расписание пользователя"""
-        schedule = cls.schedule()
-        if schedule == []:
-            return "Расписание пустое"
-        return schedule
-
-    @classmethod
-    def get_today_schedule(cls):
-        """Получаем расписание на сегодня"""
-        schedule = cls.get_schedule()
-        try:
-            return schedule[0]
-        except IndexError:
-            return "Сегодня нет пар"
+    session: Session = None
     
-    @classmethod
-    def get_tomorrow_schedule(cls):
-        """Получаем расписание на сегодня"""
-        schedule = cls.get_schedule()
-        try:
-            return schedule[1]
-        except IndexError:
-            return "Сегодня нет пар"
+    def __init__(self, login: str = "demo", password: str = "demo", proxy: dict = None, headers: dict = None) -> None:
+        """Инициализация класса
+
+        :param login: Логин
+        :param password: Пароль
+        :param proxy: Прокси
+        :param headers: Заголовки
+
+        :type login: str
+        :type password: str
+        :type proxy: dict
+        :type headers: dict
+
+        :return: None
+        :rtype: None
+
+        :Example:
+        
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_name()
+        'Студент Демонстрационный'
+        """
+
+        self.login = login
+        self.password = password
+        self.proxy = proxy
+        self.headers = headers
+        
+        self.__sign()
+
+    def __del__(self) -> None:
+        """Закрывает сессию
+        
+        :return: None
+        :rtype: None
+        
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> del lms
+        """
+
+        if self.session:
+            self.session.close()
+
+    def __sign(self) -> None:
+        """Авторизация
+
+        :return: None
+        :rtype: None
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms._LMS__sign()
+        """
+
+        headers: dict = self.headers if self.headers else {"User-Agent": UserAgent().random}
+        proxies: dict = self.proxy if self.proxy else {}
+
+        data: dict = {"popupUsername": self.login, "popupPassword": self.password}
+
+        self.session = Session()
+        self.session.headers.update(headers)
+        self.session.post(self._URL_LOGIN, data=data, proxies=proxies)
+        self.session.get(self._URL_SCHEDULE, proxies=proxies)
+        
+
+    @property
+    def cookies(self) -> dict:
+        """Возвращает куки
+
+        :return: Куки
+        :rtype: dict
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.cookies
+        """
+
+        return self.session.cookies.get_dict()
+
+    @cookies.setter
+    def get_cookies(self, cookies: dict) -> None:
+        """Устанавливает куки
+
+        :param cookies: Куки
+        :type cookies: dict
+
+        :return: None
+        :rtype: None
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_cookies = {"PHPSESSID": "demo"}
+        """
+
+        self.session.cookies.update(cookies)
+
+    def _get_soup_schedule(self) -> bs:
+        """Возвращает bs4 объект с расписанием
+
+        :return: bs4 объект с расписанием
+        :rtype: bs4.BeautifulSoup
+
+        :Example:
+        
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms._get_soup_schedule()
+
+        <html>...</html>
+        """
+
+        session: Session = Session()
+
+        response: Response = session.get(self._URL_SCHEDULE, cookies=self.cookies)
+
+        return bs(response.text, "html.parser")
+
+    def verify(self) -> bool:
+        """Проверяет авторизацию
+
+        :return: True, если авторизован, иначе False
+        :rtype: bool
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.verify()
+
+        True
+        """
+
+        soup: bs = self._get_soup_schedule()
+
+        return soup.find("div", {"class": "user-name"}) is not None
+
+    def get_name(self) -> str:
+        """Возвращает имя
+
+        :return: Имя
+        :rtype: str
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_name()
+
+        'Студент Демонстрационный'
+        """
+
+        soup: bs = self._get_soup_schedule()
+
+        name: str = soup.find("div", {"class": "user-name"}).text
+
+        return clean_data.remove_many_spaces(name)
+
+    def get_amount_messages(self) -> int:
+        """Возвращает количество непрочитанных сообщений
+
+        :return: Количество непрочитанных сообщений
+        :rtype: int
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_amount_messages()
+
+        0
+        """
+
+        soup: bs = self._get_soup_schedule()
+
+        amount_messages: str = soup.find("a", title="Личные сообщения")
+
+        if amount_messages is None:
+            return 0
+
+        return int(clean_data.remove_many_spaces(amount_messages.text))
+
+    def get_amount_notifications(self) -> int:
+        """Возвращает количество непрочитанных уведомлений
+
+        :return: Количество непрочитанных уведомлений
+        :rtype: int
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_amount_notifications()
+
+        0
+        """
+
+        soup: bs = self._get_soup_schedule()
+
+        amount_notifications: str = soup.find("a", title="Уведомления")
+
+        if amount_notifications is None:
+            return 0
+
+        return int(clean_data.remove_many_spaces(amount_notifications.text))
+
+    def get_info(self) -> dict:
+        """Возвращает информацию
+
+        :return: Информация
+        :rtype: dict
+
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_info()
+
+        {
+            "name": "Студент Демонстрационный",
+            "amount_messages": 0,
+            "amount_notifications": 0
+        }
+        """
+
+        return {
+            "name": self.get_name(),
+            "amount_messages": self.get_amount_messages(),
+            "amount_notifications": self.get_amount_notifications()
+        }
+
+    def get_schedule(self) -> dict:
+        """Возвращает расписание
+        
+        :return: Расписание
+        :rtype: list
+        
+        :Example:
+
+        >>> from Auth_LMS import LMS
+        >>> lms = LMS(login="demo", password="demo")
+        >>> lms.get_schedule()
+        
+        {
+            "date": "2021-09-13" : {
+                "time": "08:30 - 10:00",
+                "name": "Математика",
+                "classroom": "Ауд. 101",
+                "type": "Лекция",
+                "teacher": "Иванов И.И."
+            }
+        }
+        """
+
+        soup: bs = self._get_soup_schedule()
+
+        table: bs = soup.find("table", {"class": "table-list v-scrollable"})
+        shedule: dict = {}
+
+        for tr in table.find("tbody").find_all("tr"):
+            if len(tr.find_all("td")) == 1:
+                return shedule
+            if tr.find("th"):
+                date: str = clean_data.remove_many_spaces(tr.find("th").text)
+                shedule[date] = {}
+            else:
+                time: str = clean_data.remove_many_spaces(tr.find_all("td")[0].text)
+                name: str = clean_data.remove_many_spaces(tr.find_all("td")[1].text)
+                classroom: str = clean_data.remove_many_spaces(tr.find_all("td")[2].text)
+                type_: str = clean_data.remove_many_spaces(tr.find_all("td")[3].text)
+                teacher: str = clean_data.remove_many_spaces(tr.find_all("td")[4].text)
+
+                shedule[date][time] = {
+                    "name": name,
+                    "classroom": classroom,
+                    "type": type_,
+                    "teacher": teacher
+                }
+                
+        return shedule
+
+
+if __name__ == "__main__":
+    lms = LMS(login="demo", password="demo")
+    print(lms.verify())
+    print(lms.get_info())
+    print(lms.get_schedule())
