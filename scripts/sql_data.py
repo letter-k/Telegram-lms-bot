237c250b59bcf80d5d79dcdcb522fb35a4b8f291
Nikita <98780866+kotorkovsciy@users.noreply.github.com>
2023-05-07 17:57:49 +0000

    added methods for database to process messages

--- scripts/sql_data.py
+++ scripts/sql_data.py
@@ -1,4 +1,13 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    MetaData,
+    Table,
+    ForeignKey,
+    BigInteger,
+)
 
 
 class Database:
@@ -7,10 +16,21 @@ class Database:
     users: Table = Table(
         "users",
         metadata,
-        Column("id", Integer, primary_key=True),
+        Column("id", BigInteger, primary_key=True),
         Column("email", String(50)),
         Column("password", String(50)),
         Column("type_user", String(50)),
+    )
+
+    messages: Table = Table(
+        "messages",
+        metadata,
+        Column("id", Integer, primary_key=True, autoincrement=True),
+        Column("user_id", BigInteger, ForeignKey("users.id")),
+        Column("sender_name", String(50)),
+        Column("subject", String(200)),
+        Column("date", String(50)),
+        Column("url", String(50)),
     )
 
     def __init__(self, connstring: str = "sqlite:///database.db") -> None:
@@ -187,4 +207,213 @@ class Database:
         ]
         """
 
-        return self.__connection.execute(self.users.select()).fetchall()
+        result: list = self.__connection.execute(self.users.select()).fetchall()
+
+        return [
+            {"id": i[0], "email": i[1], "password": i[2], "type_user": i[3]}
+            for i in result
+        ]
+
+    def add_message(
+        self, user_id: int, sender_name: str, subject: str, date: str, url: str
+    ) -> None:
+        """Добавление сообщения
+
+        :param user_id: ID пользователя
+        :type user_id: int
+
+        :param sender_name: Имя отправителя
+        :type sender_name: str
+
+        :param subject: Тема сообщения
+        :type subject: str
+
+        :param date: Дата сообщения
+        :type date: str
+
+        :param url: Ссылка на сообщение
+        :type url: str
+
+        :return: None
+        :rtype: None
+
+        :Example:
+
+        >>> from sql_data import Database
+        >>> db = Database()
+        >>> db.add_message(1, "demo", "demo", "demo", "demo")
+        """
+
+        self.__connection.execute(
+            self.messages.insert().values(
+                user_id=user_id,
+                sender_name=sender_name,
+                subject=subject,
+                date=date,
+                url=url,
+            )
+        )
+        self.__connection.commit()
+
+    async def all_messages_user(self, user_id: int) -> list:
+        """Получение всех сообщений пользователя
+
+        :param user_id: ID пользователя
+        :type user_id: int
+
+        :return: Возвращает список всех сообщений пользователя
+        :rtype: list
+
+        :Example:
+
+        >>> from sql_data import Database
+        >>> db = Database()
+        >>> db.all_messages_user(1)
+
+        [
+            {
+                "id": 1,
+                "user_id": 1,
+                "sender_name": "demo",
+                "subject": "demo",
+                "date": "demo",
+                "url": "demo"
+            },
+        ]
+        """
+
+        result: list = self.__connection.execute(
+            self.messages.select().where(self.messages.c.user_id == user_id)
+        ).fetchall()
+
+        return [
+            {
+                "id": i[0],
+                "user_id": i[1],
+                "sender_name": i[2],
+                "subject": i[3],
+                "date": i[4],
+                "url": i[5],
+            }
+            for i in result
+        ]
+
+    async def many_messages_user(self, user_id: int, count: int) -> list:
+        """Получение нескольких сообщений пользователя
+
+        :param user_id: ID пользователя
+        :type user_id: int
+
+        :param count: Количество сообщений
+        :type count: int
+
+        :return: Возвращает список нескольких сообщений пользователя
+        :rtype: list
+
+        :Example:
+
+        >>> from sql_data import Database
+        >>> db = Database()
+        >>> db.many_messages_user(1, 1)
+
+        [
+            {
+                "id": 1,
+                "user_id": 1,
+                "sender_name": "demo",
+                "subject": "demo",
+                "date": "demo",
+                "url": "demo"
+            },
+        ]
+        """
+
+        result: list = self.__connection.execute(
+            self.messages.select()
+            .where(self.messages.c.user_id == user_id)
+            .limit(count)
+        ).fetchall()
+
+        return [
+            {
+                "id": i[0],
+                "user_id": i[1],
+                "sender_name": i[2],
+                "subject": i[3],
+                "date": i[4],
+                "url": i[5],
+            }
+            for i in result
+        ]
+
+    async def slice_messages_user(self, user_id: int, start: int, end: int) -> list:
+        """Получение нескольких сообщений пользователя
+
+        :param user_id: ID пользователя
+        :type user_id: int
+
+        :param start: Начало среза
+        :type start: int
+
+        :param end: Конец среза
+        :type end: int
+
+        :return: Возвращает список нескольких сообщений пользователя
+        :rtype: list
+
+        :Example:
+
+        >>> from sql_data import Database
+        >>> db = Database()
+        >>> db.slice_messages_user(1, 0, 1)
+
+        [
+            {
+                "id": 1,
+                "user_id": 1,
+                "sender_name": "demo",
+                "subject": "demo",
+                "date": "demo",
+                "url": "demo"
+            },
+        ]
+        """
+
+        result: list = self.__connection.execute(
+            self.messages.select()
+            .where(self.messages.c.user_id == user_id)
+            .slice(start, end)
+        ).fetchall()
+
+        return [
+            {
+                "id": i[0],
+                "user_id": i[1],
+                "sender_name": i[2],
+                "subject": i[3],
+                "date": i[4],
+                "url": i[5],
+            }
+            for i in result
+        ]
+
+    def del_all_messages_user(self, user_id: int) -> None:
+        """Удаление всех сообщений пользователя
+
+        :param user_id: ID пользователя
+        :type user_id: int
+
+        :return: None
+        :rtype: None
+
+        :Example:
+
+        >>> from sql_data import Database
+        >>> db = Database()
+        >>> db.del_all_messages_user(1)
+        """
+
+        self.__connection.execute(
+            self.messages.delete().where(self.messages.c.user_id == user_id)
+        )
+        self.__connection.commit()
